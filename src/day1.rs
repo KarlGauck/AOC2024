7094e567ad8cc1c5d78c89243bdf706733e12b63
Karl Gauck <kgauck@gmx.de>
2024-12-02 11:29:29 +0100

    day1 part2

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,17 @@
 use std::fs;
 
-pub fn read_input(){
+fn read_input() -> (Vec<i32>, Vec<i32>)
+{
     let content = fs::read_to_string("inputs/day1.txt");
     let content = content.unwrap_or("0".to_string());
     let content = content.split('\n').collect::<Vec<_>>();
-    let mut list1 = content.iter().map(|s| s.split("   ").collect::<Vec<&str>>()[0]).collect::<Vec<&str>>().iter().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-    let mut list2 = content.iter().map(|s| s.split("   ").collect::<Vec<&str>>()[1]).collect::<Vec<&str>>().iter().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let list1 = content.iter().map(|s| s.split("   ").collect::<Vec<&str>>()[0]).collect::<Vec<&str>>().iter().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let list2 = content.iter().map(|s| s.split("   ").collect::<Vec<&str>>()[1]).collect::<Vec<&str>>().iter().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    (list1, list2)
+}
+
+pub fn part1() {
+    let (mut list1, mut list2) = read_input();
     list1.sort();
     list2.sort();
     let mut sum = 0;
@@ -13,4 +19,20 @@ pub fn read_input(){
         sum += (list1[i] - list2[i]).abs();
     }
     println!("{}", sum)
+}
+
+pub fn part2() {
+    let (list1,list2) = read_input();
+    let overlap = list1.iter().filter(|e| list2.contains(*e)).collect::<Vec<_>>();
+    for e in &overlap {
+        println!("{}", e);
+    }
+    let mut sum = 0;
+    for elem in overlap {
+        let val: i32 = list2.iter().filter(|e| *e == elem).collect::<Vec<_>>().len() as i32;
+        println!("{}", val);
+        sum += val * elem;
+    }
+
+    println!("{}", sum);
 }
