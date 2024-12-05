68f6af9328d5f236e904db4f88ae73352612caad
Karl Gauck <kgauck@gmx.de>
2024-12-05 11:00:51 +0100

    day5 part2

--- src/day5.rs
+++ src/day5.rs
@@ -28,3 +28,51 @@ pub fn part1() {
     }
     println!("Day 5 Part 1: {}", sum);
 }
+
+pub fn part2() {
+    let (rules, updates) = read_input();
+    let mut sum = 0;
+
+    let mut incorrect_updates: Vec<Vec<i32>> = vec![];
+
+    'updates: for update in updates {
+        for rule in rules.iter() {
+            if !update.contains(&rule.0) {
+                continue;
+            }
+            let first_index = update.iter().position(|&r| r == rule.0).unwrap();
+            let second_index = update.iter().position(|&r| r == rule.1).unwrap_or(usize::MAX);
+            if first_index > second_index {
+                incorrect_updates.push(update);
+                continue 'updates;
+            }
+        }
+    }
+
+    for mut update in &mut incorrect_updates.into_iter() {
+        let relevant_rules = rules.clone();
+        let mut relevant_rules = relevant_rules.iter().filter(|(a, b)| update.contains(a)).collect::<Vec<&(i32,i32)>>();
+
+        let mut illegal = true;
+        'correct: while illegal {
+            illegal = false;
+
+            for rule in relevant_rules.iter() {
+                if !update.contains(&rule.0) {
+                    continue;
+                }
+                let first_index = update.iter().position(|&r| r == rule.0).unwrap();
+                let second_index = update.iter().position(|&r| r == rule.1).unwrap_or(usize::MAX);
+                if second_index < first_index {
+                    update.swap(first_index, second_index);
+                    illegal = true;
+                    continue 'correct;
+                }
+            }
+        }
+
+        sum += update[(update.len()-1)/2];
+    }
+
+    println!("Day 5 Part 2: {}", sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,5 @@ mod day5;
 
 fn main() {
     day5::part1();
+    day5::part2();
 }
